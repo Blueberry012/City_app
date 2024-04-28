c27b6d55d47d3cd97aa2ca7f67c6c909ab1c9cd6
Blueberry012 <156487524+Blueberry012@users.noreply.github.com>
2024-04-28 14:17:37 +0200

    Update 4_Ressources.py

--- pages/4_Ressources.py
+++ pages/4_Ressources.py
@@ -7,17 +7,17 @@ st.image("image/map.png", width=250, use_column_width=False, clamp=True)
 
 def afficher_data_dep():
     st.header("Données par département")
-    data_dep = pd.read_excel('data\data_departement.xlsx')
+    data_dep = pd.read_excel('data/data_departement.xlsx')
     st.write(data_dep)
 
 def afficher_data_meteo():
     st.header("Données météorologiques")
-    data_meteo = pd.read_excel('data\meteo.xlsx')
+    data_meteo = pd.read_excel('data/meteo.xlsx')
     st.write(data_meteo)
 
 def afficher_data_ville():
     st.header("Données par ville")
-    data_ville = pd.read_excel('data\data_ville.xlsx')
+    data_ville = pd.read_excel('data/data_ville.xlsx')
     st.write(data_ville)
 
 afficher_data_dep()
